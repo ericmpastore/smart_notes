77b004ea705442954aba88252e31822467499f09
Eric P <epas001@hotmail.com>
2026-05-21 21:53:57 +0200

    Added popular notes list view

--- notes/views.py
+++ notes/views.py
@@ -10,6 +10,12 @@ class NotesListView(ListView):
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
 
+class PopularNotesListView(ListView):
+    model = Notes
+    context_object_name = "notes"
+    template_name = "notes/notes_list.html"
+    queryset = Notes.objects.filter(count_likes__gte=1)
+
 class NotesDetailView(DetailView):
     model = Notes
     context_object_name = "note"
