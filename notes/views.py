4218a305e41fc540dab32494d5a18d42501a1c0a
Eric P <epas001@hotmail.com>
2026-06-01 22:17:25 +0200

    Added form validation

--- notes/views.py
+++ notes/views.py
@@ -3,6 +3,7 @@ from django.http import Http404, HttpResponseRedirect
 from .forms import NotesForm
 from django.views.generic import CreateView, ListView, DetailView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 
 # Create your views here.
@@ -26,15 +27,25 @@ class NotesCreateView(CreateView):
     success_url = '/smart/notes'
     form_class = NotesForm
 
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
 class NotesUpdateView(UpdateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm   
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin,ListView):
     model = Notes
     context_object_name = "notes"
     template_name = "notes/notes_list.html"
+    login_url = "/admin"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class PopularNotesListView(ListView):
     model = Notes
