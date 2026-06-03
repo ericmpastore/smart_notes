9621f58e55be2d2f318343b07e3011cb92d41344
Eric P <epas001@hotmail.com>
2026-06-03 18:25:46 +0200

    added login and logout functions

--- home/views.py
+++ home/views.py
@@ -3,11 +3,18 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
 
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
     extra_context = {'today': datetime.today()}
 
-class AuthorizedView(LoginRequiredMixin,TemplateView):
-    template_name = 'home/authorized.html'
-    login_url = '/admin'
+class LoginInterfaceView(LoginView):
+    template_name = 'home/login.html'
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'
+    http_method_names = ['get', 'post', 'options']
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
